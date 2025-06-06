75ed7913c787ff8f7e587225408858a6fcde9b23
Alin Istratescu <alinistratescu@yahoo.com>
2025-06-06 23:48:04 +0300

    delete unused project files

--- My misteries project/add_motive_column.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('mysteries.db')
-c = conn.cursor()
-c.execute("ALTER TABLE suspects ADD COLUMN motive TEXT")
-conn.commit()
-conn.close()
-print("Added 'motive' column to suspects table.")
