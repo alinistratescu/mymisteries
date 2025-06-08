e8fccccd3efb2cd614ff523aaca906c755e26f39
Alin Istratescu <alinistratescu@yahoo.com>
2025-06-09 00:50:24 +0300

    Add dynamic mistery case

--- fix_real_killer.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('mysteries.db')
-c = conn.cursor()
-# Set the real killer for the first case to 'John Miller' (who is a suspect)
-c.execute("UPDATE real_killer SET name=? WHERE case_id=0", ("John Miller",))
-conn.commit()
-conn.close()
-print("Real killer for case 0 set to 'John Miller'.")
